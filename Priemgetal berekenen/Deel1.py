1cc19de2d9dc21912646a34d9092983265eb1811
DimiCalu <dimitri.caluwaerts@outlook.com>
2019-11-13 20:52:43 +0100

    Update Deel1.py

--- Priemgetal berekenen/Deel1.py
+++ Priemgetal berekenen/Deel1.py
@@ -1,6 +1,19 @@
 def is_prime(number):
-    for item in range(2):
+    if(number > 1):
+        number_is_prime = True
+        for i in range(2, number):
+            if(number%i == 0):
+                number_is_prime = False
+                break
+        return number_is_prime
+    else:
+        return False
         
 
 print(is_prime(13)) # prints True
 print(is_prime(4)) # prints False
+
+#code om meerdere getallen te testen
+
+#for i in range(2,50):
+#    print(str(i) + ": " + str(is_prime(i)))
